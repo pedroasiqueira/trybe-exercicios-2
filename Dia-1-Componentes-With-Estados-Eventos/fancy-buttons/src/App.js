8597cc2aca682f3418d1b59ed605b12df7bc8968
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-04-01 15:05:23 -0300

    Implementando o this.state e this.setState para fazer as mudanças na página quando clico

--- Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
+++ Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
@@ -1,42 +1,29 @@
 import React, { Component } from 'react'
 import './App.css';
 
-// const clique1 = () => console.log('Na página não acontece nada, só aqui 1')
-// const clique2 = () => console.log('Igual ao anterior, só muda o texto 2')
-// const clique3 = () => console.log('Mesma coisa 3')
 
 class App extends Component {
   constructor() {
     super()
+    this.clique1 = this.clique1.bind(this)
+    this.state = {
+      numeroDeCliques: 0
+    }
 
-    this.clique2 = this.clique2.bind(this)
-
-    console.log('teste');
   }
   clique1() {
-    //O this não vai aparecer no navegador porque eu não coloquei essa função dentro do constructor e abaixo do super.
-    console.log(this)
-    console.log('Na página não acontece nada, só aqui 1')
-}
-  clique2() {
-    // O this aparece no navegador porque eu coloquei essa função dentro do constructor e abaixo do super.
-    console.log(this)
-    console.log('Igual ao anterior, só muda o texto 2')
-  }
-  clique3 = () => {
-    // O this vai aparecer porque essa é uma arrow function. E não precisa colocar dentro do constructor
-    console.log(this)
-    console.log('Mesma coisa 3')
+    console.log('as coisas irão acontecer após essa mensagem');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
 }
 
   render() {
-    // console.log(this)
+    console.log(this)
     return (
-      <div>
-        <button onClick={this.clique1}>Clique para ver o que acontece</button>
-        <button onClick={this.clique2}>Clique para ver</button>
-        <button onClick={this.clique3}>Clica novamente papai</button>
-      </div>
+      
+        <button onClick={this.clique1}>Clique para aumentar a contagem de cliques: "{this.state.numeroDeCliques}"</button>
+      
     )
   }
 }
