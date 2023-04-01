b99de63858e6c25e560cf71c51b5a62c57fda4e6
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-04-01 14:12:01 -0300

    Fazendo os exemplos dos this dentro do constructor e abaixo da super() e usando arrow function também. PAra deixar claro as formas como posso fazer

--- Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
+++ Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
 import './App.css';
 
-const clique1 = () => console.log('Na página não acontece nada, só aqui 1')
-const clique2 = () => console.log('Igual ao anterior, só muda o texto 2')
-const clique3 = () => console.log('Mesma coisa 3')
+// const clique1 = () => console.log('Na página não acontece nada, só aqui 1')
+// const clique2 = () => console.log('Igual ao anterior, só muda o texto 2')
+// const clique3 = () => console.log('Mesma coisa 3')
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.clique2 = this.clique2.bind(this)
+
+    console.log('teste');
+  }
+  clique1() {
+    //O this não vai aparecer no navegador porque eu não coloquei essa função dentro do constructor e abaixo do super.
+    console.log(this)
+    console.log('Na página não acontece nada, só aqui 1')
+}
+  clique2() {
+    // O this aparece no navegador porque eu coloquei essa função dentro do constructor e abaixo do super.
+    console.log(this)
+    console.log('Igual ao anterior, só muda o texto 2')
+  }
+  clique3 = () => {
+    // O this vai aparecer porque essa é uma arrow function. E não precisa colocar dentro do constructor.
+    console.log(this)
+    console.log('Mesma coisa 3')
+}
+
   render() {
+    // console.log(this)
     return (
       <div>
-        <button onClick={clique1}>Clique para ver o que acontece</button>
-        <button onClick={clique2}>Clique para ver</button>
-        <button onClick={clique3}>Clica novamente papai</button>
+        <button onClick={this.clique1}>Clique para ver o que acontece</button>
+        <button onClick={this.clique2}>Clique para ver</button>
+        <button onClick={this.clique3}>Clica novamente papai</button>
       </div>
     )
   }
