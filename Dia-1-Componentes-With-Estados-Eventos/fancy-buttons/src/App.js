85a426a9474e8c97b92e489529b3bd1477037e2d
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-04-03 15:18:01 -0300

    Feito os botões para aumentar de 5 e 10

--- Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
+++ Dia-1-Componentes-With-Estados-Eventos/fancy-buttons/src/App.js
@@ -6,8 +6,12 @@ class App extends Component {
   constructor() {
     super()
     this.clique1 = this.clique1.bind(this)
+    this.clique5 = this.clique5.bind(this)
+    this.clique10 = this.clique10.bind(this)
     this.state = {
-      numeroDeCliques: 0
+      numeroDeCliques: 0,
+      numeroDeCliques5: 0,
+      numeroDeCliques10: 0,
     }
 
   }
@@ -18,20 +22,35 @@ class App extends Component {
     }))
 }
 
-clique5() {
-  console.log('as coisas irão acontecer após essa mensagem');
-  this.setState((estadoAnterior, _props) => ({
-    numeroDeCliques: estadoAnterior.numeroDeCliques + 5
-  }))
-}
+  clique5() {
+    console.log('as coisas irão acontecer após essa mensagem');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques5: estadoAnterior.numeroDeCliques5 + 5
+    }))
+  }
+
+  clique10() {
+    console.log('as coisas irão acontecer após essa mensagem');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques10: estadoAnterior.numeroDeCliques10 + 10
+    }))
+  }
 
   render() {
     // console.log(this)
     const { numeroDeCliques } = this.state
+    const { numeroDeCliques5 } = this.state
+    const { numeroDeCliques10 } = this.state
     return (
       <div>
-        <button onClick={this.clique1}>Clique para aumentar a contagem de cliques</button>
+        <button onClick={this.clique1}>Clique para aumentar de 1 em 1</button>
         <p>{numeroDeCliques}</p>
+
+        <button onClick={this.clique5}>Clique para aumentar de 5 em 5</button>
+        <p>{numeroDeCliques5}</p>
+
+        <button onClick={this.clique10}>Clique para aumentar de 10 e 10</button>
+        <p>{numeroDeCliques10}</p>
       </div>
     )
   }
--- Dúvidas.md
+++ Dúvidas.md
@@ -1,5 +1,4 @@
 Vai ser melhor usar arrow function para não ter que digitar mais dentro do constructor? Ou usando o arrow function vai ter uma restrição no futuro e talvez seja melhor usar a função normal para ter que colocar dentro do constructor a nossa função?
 
 
-Fazer outro botão que some de 5 em 5
 Criar um componente Botão, que eu possa reutiliza-lo e fazer vários botões facilmente
