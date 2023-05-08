061ade3a0b02501d1a974eb01b16720602666a01
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-05-08 10:26:28 -0300

    adc

---
+++ Dia-2-react-no-redux/readme.md
@@ -0,0 +1,6 @@
+intalando redux no React
+```javascript
+npm install redux react-redux
+```
+
+O RESPONSÁVEL por alterar o Estado Global no Redux é o `Reducer`.
---
+++ Dia-2-react-no-redux/redux-no-react/src/redux/reducers/counterReducer.js
@@ -0,0 +1,20 @@
+const INITIAL_STATE = {
+  count: 0,
+  clicks: 0,
+}
+
+const counterReducer = (state = INITIAL_STATE, action) => {
+
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+  
+    default:
+      return state;
+  }
+}
+
+export default counterReducer;
